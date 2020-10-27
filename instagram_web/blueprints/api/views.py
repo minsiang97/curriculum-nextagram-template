a304e34ccbe9622b9a34dc0e48cacd132dece4c9
Ong Min Siang <ongminsiang@gmail.com>
2020-10-27 16:37:42 +0800

    added token

--- instagram_web/blueprints/api/views.py
+++ instagram_web/blueprints/api/views.py
@@ -1,12 +1,18 @@
 from flask import Blueprint, render_template, flash, request, redirect, url_for, session, jsonify
+from flask_login import login_required, current_user, login_user
 from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity, get_jwt_claims
 
 api_blueprint = Blueprint('api',
                             __name__,
                             template_folder='templates')
 
-@api_blueprint.route('/create_token', methods=['POST'])
+@api_blueprint.route('/create_token')
 def create():
-    access_token = create_access_token(identity=current_user.id)
-    ret = {'access_token': access_token}
-    return jsonify (ret)
+    return jsonify(create_access_token(identity="user_id"))
+
+@api_blueprint.route('/receive_token')
+@jwt_required
+def receive():
+    identity = get_jwt_identity()
+    return(identity)
+
